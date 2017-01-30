be30804b550b703d0db13a475f3008ab341dfe8c
OAntoshchuk <zzalex.km@gmail.com>
2017-01-30 17:02:54 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,39 +5,11 @@
   <title>Home</title>
    </head>
  <body>
-    <h1>Home</h1>
-   	<h1><a href="contact.php"> contact us </a></h1>
-   	<br><br>
 
-
-
-<?php
-require 'connect.php';
-
-$in = ($_GET['in']);
-if($in =='contact'){
-header("Location: contact.php");
-}
-
-
-
-
-$result = mysqli_query($link,"SELECT * FROM `message` ");
-mysqli_close($link);
-
-
-//echo $row['name'];
-
-while ($row = mysqli_fetch_array($result))
-{   ?>
-
-<h1><?php echo $row['name'];?></h1>
-	<p><?php echo $row['mail'];?></p>
-	<p><?php echo $row['description'];?></p>
-
- <?php  }   ?>
-
- 
+ <?php
+   	include "menu.php";
+   	//include "contact.php";
+   	?>
 
 
  </body>
---
+++ menu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Home</title>
+   </head>
+ <body>
+    <h1>Home</h1>
+   	<h1><a href="contact.php"> contact us </a></h1>
+   	<br><br>
+
+
+
+<?php
+require 'connect.php';
+
+//$in = ($_GET['in']);
+//if($in =='contact'){
+//header("Location: contact.php");
+//}
+
+
+
+
+$result = mysqli_query($link,"SELECT * FROM `message` ");
+mysqli_close($link);
+
+
+//echo $row['name'];
+
+while ($row = mysqli_fetch_array($result))
+{   ?>
+
+<h1><?php echo $row['name'];?></h1>
+	<p><?php echo $row['mail'];?></p>
+	<p><?php echo $row['description'];?></p>
+
+ <?php  }   ?>
+
+ 
+
+
+ </body>
+</html>
