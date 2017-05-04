efae3b830c4efa1ff9c94383bb7abd82586bae89
OAntoshchuk <zzalex.km@gmail.com>
2017-05-04 20:18:30 +0300

    Add files via upload

---
+++ samplebd.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 04 2017 г., 19:16
+-- Версия сервера: 5.5.39
+-- Версия PHP: 5.4.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `samplebd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `name` text NOT NULL,
+  `mail` text NOT NULL,
+  `description` text NOT NULL,
+`id` int(128) NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+--
+-- Дамп данных таблицы `message`
+--
+
+INSERT INTO `message` (`name`, `mail`, `description`, `id`) VALUES
+('qwerty', 'asdfg', 'zxcvb', 1),
+('qwerty', 'asdfg', 'zxcvb', 2),
+('zeb', 'joji', 'drd', 3),
+('wery', 'sdfg', 'xcvb', 4),
+('hjghjg gh', 'hgf ghfgfbvghvg', 'gvgvg', 5),
+('ddd', 'dddd', 'dddddd', 6),
+('ddd', 'dddd', 'dddddd', 7),
+('sasa', 'dada', 'uyuiyuiyuy yiyyy yyiuyiyy yy yiuyuyuyuyuyyu y i', 8),
+('alex', 'as@mail.ru', 'fbyfbtgfgyfgfbgfb ffbf nuynygny', 9),
+('dfsfs', 'sdfsdf', 'dsfdsfdsfds', 10),
+('', '', '', 11),
+('gfhgfg', 'hghjgjhgbh', 'bhb guy ', 12),
+('jkjkb', 'hbhbhb', 'jnjkbjkbb', 13),
+('kjhbhbb', 'jkbhbhj', 'hbhbhb', 14),
+('jknjkn', 'kjkljmkl', 'jb g uyg yuy fuy ', 15);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+MODIFY `id` int(128) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
